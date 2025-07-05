060360b69e20e2cdbd0723201e8944af31ea4cd1
Mr Smile Modds <uceyheroku@gmail.com>
2025-07-05 21:30:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "POPKID;;;TIZS1bRB#tdEuL2kc47iAhItPKKQ_XaqkKLXp8zcqfR9_BoFxsQ8",
+SESSION_ID: process.env.SESSION_ID || "SMILES~MvFixbwB#-G-m9qS8fkVJNoE_IRLYmHHOfV_Bvh6R9eXexqj8Jvg",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
